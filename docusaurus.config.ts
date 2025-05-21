9823b5f23a9503e35ed2e0da05847a1194adf264
kildemal <206095739+izrofid@users.noreply.github.com>
2025-05-21 11:21:56 +0000

    change config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,17 +10,18 @@ const config: Config = {
   favicon: 'img/empoleonlogo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://izrofid.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/eiweb',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'izrofid', // Usually your GitHub org/user name.
+  projectName: 'eiweb', // Usually your repo name.
 
   onBrokenLinks: 'throw',
+  trailingSlash: false,
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
